d103f02a2bfe97ea78906bf07e99ac67d0357538
SimplicioDias <simplicio.g3@gmail.com>
2024-01-05 01:32:52 -0300

    raycasting implement

--- player.py
+++ player.py
@@ -31,17 +31,29 @@ class Player:
             dx += -speed_sin
             dy += speed_cos
             
-        self.x += dx
-        self.y += dy
+        self.check_wall_collision(dx,dy)
         
         if keys[pg.K_LEFT]:
             self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
         if keys[pg.K_RIGHT]:
             self.angle += PLAYER_ROT_SPEED * self.game.delta_time
         self.angle %= math.tau # tau = 2*pi
+    
+    
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+    
+    def check_wall_collision(self,dx,dy):
+        if self.check_wall(int(self.x + dx), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy)):
+            self.y += dy
+        
+        
         
     def draw(self):
-        pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100), (self.x * 100 + WIDTH * math.cos(self.angle), self.y*100 + WIDTH * math.sin(self.angle)), 2)
+        
+        """ pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100), (self.x * 100 + WIDTH * math.cos(self.angle), self.y*100 + WIDTH * math.sin(self.angle)), 2) """
         
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
         
